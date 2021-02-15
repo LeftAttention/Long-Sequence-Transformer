f59c200bfa813e4117880c7cf4b9de3a3594b08c
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-02-15 22:35:44 +0530

    Update stopping.py

--- utils/stopping.py
+++ utils/stopping.py
@@ -1,2 +1,11 @@
 import numpy as np
 import torch
+
+def adjust_learning_rate(optimizer, epoch, learning_rate):
+    # lr = learning_rate * (0.2 ** (epoch // 2))
+    
+    lr_adjust = learning_rate * (0.5 ** ((epoch-1) // 1))
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr_adjust
+    print('Updating learning rate to {}'.format(lr_adjust))
