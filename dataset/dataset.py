7e1474cbee5bd016b339eeb0d9d11d4075ff61ae
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-02-15 20:48:54 +0530

    Update dataset.py

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -8,3 +8,18 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 import warnings
 warnings.filterwarnings('ignore')
+
+class Days_dataset(Dataset):
+    
+    def __init__(self, root, mode='train', scale=True, x_window=96, y_window=72, inter=48):
+        
+        assert mode in ['train', 'test', 'val']
+        
+        self.root = root
+        self.scale = scale
+        self.x_window = x_window
+        self.y_window = y_window
+        self.inter = inter
+        
+        self._read_data()
+        
