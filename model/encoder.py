b69059898f899ff6af4aa16dab3585159ba242b8
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-02-16 14:47:19 +0530

    Update encoder.py

--- model/encoder.py
+++ model/encoder.py
@@ -46,3 +46,26 @@ class EncoderLayer(nn.Module):
         y = self.dropout(self.conv2(y).transpose(-1,1))
 
         return self.norm2(x+y)
+    
+class Encoder(nn.Module):
+    def __init__(self, attn_layers, conv_layers=None, norm_layer=None):
+        super(Encoder, self).__init__()
+        self.attn_layers = nn.ModuleList(attn_layers)
+        self.conv_layers = nn.ModuleList(conv_layers) if conv_layers is not None else None
+        self.norm = norm_layer
+
+    def forward(self, x, attn_mask=None):
+        # x [B, L, D]
+        if self.conv_layers is not None:
+            for attn_layer, conv_layer in zip(self.attn_layers, self.conv_layers):
+                x = attn_layer(x, attn_mask=attn_mask)
+                x = conv_layer(x)
+            x = self.attn_layers[-1](x)
+        else:
+            for attn_layer in self.attn_layers:
+                x = attn_layer(x, attn_mask=attn_mask)
+
+        if self.norm is not None:
+            x = self.norm(x)
+
+        return x
