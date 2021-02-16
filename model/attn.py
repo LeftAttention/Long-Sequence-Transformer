d2242990473e04330382c14751f66a5311d242f6
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-02-16 14:36:00 +0530

    Update attn.py

--- model/attn.py
+++ model/attn.py
@@ -108,3 +108,36 @@ class ProbAttention(nn.Module):
         context = self._update_context(context, values, scores_top, index, L, attn_mask)
         
         return context.contiguous()
+    
+class AttentionLayer(nn.Module):
+    def __init__(self, attention, d_model, n_heads, d_keys=None,
+                 d_values=None):
+        super(AttentionLayer, self).__init__()
+
+        d_keys = d_keys or (d_model//n_heads)
+        d_values = d_values or (d_model//n_heads)
+
+        self.inner_attention = attention
+        self.query_projection = nn.Linear(d_model, d_keys * n_heads)
+        self.key_projection = nn.Linear(d_model, d_keys * n_heads)
+        self.value_projection = nn.Linear(d_model, d_values * n_heads)
+        self.out_projection = nn.Linear(d_values * n_heads, d_model)
+        self.n_heads = n_heads
+
+    def forward(self, queries, keys, values, attn_mask):
+        B, L, _ = queries.shape
+        _, S, _ = keys.shape
+        H = self.n_heads
+
+        queries = self.query_projection(queries).view(B, L, H, -1)
+        keys = self.key_projection(keys).view(B, S, H, -1)
+        values = self.value_projection(values).view(B, S, H, -1)
+
+        out = self.inner_attention(
+            queries,
+            keys,
+            values,
+            attn_mask
+        ).view(B, L, -1)
+
+        return self.out_projection(out)
