2c96c28859c853f0dab9f3d90d7e32d35f673c24
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-02-16 14:55:22 +0530

    Update model.py

--- model/model.py
+++ model/model.py
@@ -7,3 +7,11 @@ from models.encoder import Encoder, EncoderLayer, ConvLayer
 from models.decoder import Decoder, DecoderLayer
 from models.attn import FullAttention, ProbAttention, AttentionLayer
 from models.embedding import DataEmbedding
+
+class LongTimeFormer(nn.Module):
+    
+    def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len, 
+                factor=5, d_model=512, n_heads=8, e_layers=3, d_layers=2, d_ff=512, 
+                dropout=0.0, attn='prob', embed='fixed', activation='gelu', 
+                device=torch.device('cuda:0')):
+        super(LongTimeFormer, self).__init__()
