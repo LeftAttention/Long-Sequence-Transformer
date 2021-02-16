db107161b072415c473e0c3c8fa7efcc4ad490f7
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-02-16 18:23:47 +0530

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -73,3 +73,66 @@ class Assistant(object):
         
         total_loss = np.average(total_loss)
         return total_loss
+
+    def train(self):
+        if not os.path.exists(self.config.weight_path):
+            os.makedirs(self.config.weight_path)
+            
+        time_now = time.time()
+        
+        train_steps = len(self.train_loader)
+        early_stopping = EarlyStopping(patience=self.config.patience, verbose=True)
+        
+        for epoch in range(self.config.train_epochs):
+            iter_count = 0
+            train_loss = []
+            
+            self.change_mode('train')
+            for i, (batch_x,batch_y,batch_x_mark,batch_y_mark) in enumerate(self.train_loader):
+                iter_count += 1
+                
+                self.model_optim.zero_grad()
+                
+                batch_x = batch_x.double().to(self.device)
+                batch_y = batch_y.double()
+                
+                batch_x_mark = batch_x_mark.double().to(self.device)
+                batch_y_mark = batch_y_mark.double().to(self.device)
+
+                # decoder input
+                dec_inp = torch.zeros_like(batch_y[:,-self.args.pred_len:,:]).double()
+                dec_inp = torch.cat([batch_y[:,:self.args.label_len,:], dec_inp], dim=1).double().to(self.device)
+                # encoder - decoder
+                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+
+                batch_y = batch_y[:,-self.args.pred_len:,:].to(self.device)
+                loss = self.criterion(outputs, batch_y)
+                train_loss.append(loss.item())
+                
+                if (i+1) % 100==0:
+                    print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
+                    speed = (time.time()-time_now)/iter_count
+                    left_time = speed*((self.config.train_epochs - epoch)*train_steps - i)
+                    print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
+                    iter_count = 0
+                    time_now = time.time()
+                    
+                loss.backward()
+                self.model_optim.step()
+
+            train_loss = np.average(train_loss)
+            val_loss = self.validate()
+            
+            print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Val Loss: {3:.7f}".format(
+                epoch + 1, train_steps, train_loss, val_loss))
+            early_stopping(val_loss, self.model, self.config.weight_path)
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
+
+            adjust_learning_rate(self.model_optim, epoch+1, self.config.learning_rate)
+            
+        best_model_path = self.config.weight_path+'/'+'checkpoint.pth'
+        self.model.load_state_dict(torch.load(best_model_path))
+        
+        return self.model
