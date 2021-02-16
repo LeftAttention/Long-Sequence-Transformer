75b99b98e40b54a8f0591c5e13a1e85977133fe5
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-02-16 17:30:32 +0530

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -12,3 +12,33 @@ from model.model import LongTimeFormer
 
 from utils.stopping import EarlyStopping, adjust_learning_rate
 from utils.metrics import metric
+
+class Assistant(object):
+    
+    def __init__(self, config):
+        
+        self.model = LongTimeFormer(
+            config.enc_in,
+            config.dec_in, 
+            config.c_out, 
+            config.seq_len, 
+            config.label_len,
+            config.pred_len, 
+            config.factor,
+            config.d_model, 
+            config.n_heads, 
+            config.e_layers,
+            config.d_layers, 
+            config.d_ff,
+            config.dropout, 
+            config.attn,
+            config.embed,
+            config.data[:-1],
+            config.activation,
+            config.device,
+        ).double()
+        
+        self.criterion =  nn.MSELoss()
+        self.model_optim = optim.Adam(self.model.parameters(), lr=config.learning_rate)
+        
+  
